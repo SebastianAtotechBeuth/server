54f11292cbaab1e840e9eee21069039150f1c614
SebastianAtotechBeuth <hansi987!>
2015-12-17 17:13:01 +0100

    upload.php created

--- info.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-var_dump($_REQUEST);
-echo "<hr>";
-var_dump($_FILE);
-echo "<hr>";
-phpinfo();
-
-?>
---
+++ upload.php
@@ -0,0 +1,6 @@
+<?php
+
+echo "simple var_dump echo of \$_request and \$_files <hr>";
+var_dump( $_REQUEST );
+echo "<hr>";
+var_dump( $_FILES );
